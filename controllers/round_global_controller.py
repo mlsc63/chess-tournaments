48f0a30d27fff6ff1c2f743632e6e009c3ae934b
Maxime <maxim.lescure63@gmail.com>
2021-02-02 15:12:49 +0100

    add todo

--- controllers/round_global_controller.py
+++ controllers/round_global_controller.py
@@ -4,8 +4,14 @@ class RoundGlobalController:
 
     def __call__(self, *args, **kwargs):
 
-        # si c'est le premier round
-           #
+        # faire deux liste, 1 --> première motié, 2 ---> deuxième moitié
+        # Si round 1 Mettre dans le tuple de match la permiere moitié vs la deuxième
+        # SI round > 1 Mettre dans le tuple de match la permiere moitié vs la deuxième si ils n'ont jamias joué enssemble
+        # Demander et enregistrer les scores.
+        # Chnager le status du round -- > False
+        # si c'est le dernier round, update score et changer le status du tournois --> False
+
+
 
         print('___Enregistrement des résultats___')
 
